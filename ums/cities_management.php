1e4621d0c2740d71fd7cbd7e95135430686672e0
Marjan ghazi <syedmarjanghazi@gmail.com>
2025-11-29 10:50:09 +0500

    updated

--- ums/cities_management.php
+++ ums/cities_management.php
@@ -23,6 +23,9 @@ if (isset($_POST['add_city'])) {
         $message_type = "error";
     }
     $stmt->close();
+     // Redirect to avoid resubmission
+    header("Location: cities_management.php");
+    exit();
 }
 
 // Update city
